1962fac7c47d371af6e4337cd7eab9df8ff2d45f
Ahmetbasli <68350525+serywin@users.noreply.github.com>
2021-09-08 21:13:29 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      {/* header */}
+      <Router>
+        <Switch>
+          {/* home */}
+          <Route exact path="/">
+            <h1>home</h1>
+          </Route>
+
+          {/* contactUs */}
+          <Route path="/contact">
+            <h1>contact</h1>
+          </Route>
+        </Switch>
+      </Router>
+      {/* footer */}
     </div>
   );
 }
