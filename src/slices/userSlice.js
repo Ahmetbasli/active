dfe3bea837ebf595dd3d638bf43c2f5df74e33ac
Ahmetbasli <68350525+serywin@users.noreply.github.com>
2021-09-10 21:10:23 +0300

    Update store to toggle modal

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  user: null,
   page: "home",
   language: navigator.language.split("-")[0],
+  IsLoginModalOpen: false,
 };
 
 export const userSlice = createSlice({
@@ -17,12 +19,25 @@ export const userSlice = createSlice({
       console.log(state.language);
       state.language = action.payload;
     },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    toggleIsLoginModalOpen: (state, action) => {
+      state.IsLoginModalOpen = action.payload;
+    },
   },
 });
 
-export const { adjustWhichPage, adjustLanguage } = userSlice.actions;
+export const {
+  adjustWhichPage,
+  adjustLanguage,
+  setUser,
+  toggleIsLoginModalOpen,
+} = userSlice.actions;
 
 export const selectWhichPage = (state) => state.user.page;
 export const selectLanguage = (state) => state.user.language;
+export const selectUser = (state) => state.user.user;
+export const selectIsLoginModalOpen = (state) => state.user.IsLoginModalOpen;
 
 export default userSlice.reducer;
